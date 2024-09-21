9d7bc0dd2eb7a523f18ef72cf1587448a0315783
gergely.szecsenyi <gergely@szecsenyi.net>
2024-09-21 09:59:37 +0200

    add AzureOpenAI Support

--- src/ask/llm-support.ts
+++ src/ask/llm-support.ts
@@ -1,6 +1,8 @@
 
 import { LanguageModelLike } from '@langchain/core/language_models/base';
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
+import { AzureChatOpenAI, AzureOpenAIEmbeddings } from "@langchain/openai";
+
 import { EmbeddingsInterface } from '@langchain/core/embeddings';
 export class LLM {
     static define_ChatOpenAI(model: string, apiKey:string, temperature = 0): LanguageModelLike {
@@ -11,6 +13,27 @@ export class LLM {
           });
 
           return llm;
+    }
+
+    static define_AzureOpenAI(deploymentName: string, apiKey:string, apiVersion:string, instanceName:string, temperature = 0): LanguageModelLike {
+        const llm = new AzureChatOpenAI({
+            temperature: temperature,
+            azureOpenAIApiDeploymentName: deploymentName,
+            azureOpenAIApiKey: apiKey,
+            azureOpenAIApiVersion: apiVersion,
+            azureOpenAIApiInstanceName: instanceName,
+          });
+
+        return llm;
+    }
+
+    static define_AzureOpenAIEmbeddings(deploymentName: string, apiKey:string, apiVersion:string, instanceName:string): EmbeddingsInterface {
+        const embeddings = new AzureOpenAIEmbeddings({
+            apiKey: apiKey, 
+            deploymentName: deploymentName, 
+            azureOpenAIApiInstanceName: instanceName,
+            openAIApiVersion: apiVersion})
+        return embeddings;
     }
 
     static define_OpenAIEmbeddings(apiKey:string): EmbeddingsInterface {
